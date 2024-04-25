5121c001c1e61e803cbd5f81451e3e9746486f90
0xGuih <owensempai@gmail.com>
2024-04-25 20:06:59 -0300

    Users

---
+++ Repository/Migrations/20240425225418_Second.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Standings_Clubs_ClubId",
+                table: "Standings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Standings_Tournaments_TournamentId",
+                table: "Standings");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Standings_Clubs_ClubId",
+                table: "Standings",
+                column: "ClubId",
+                principalTable: "Clubs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Standings_Tournaments_TournamentId",
+                table: "Standings",
+                column: "TournamentId",
+                principalTable: "Tournaments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Standings_Clubs_ClubId",
+                table: "Standings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Standings_Tournaments_TournamentId",
+                table: "Standings");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Standings_Clubs_ClubId",
+                table: "Standings",
+                column: "ClubId",
+                principalTable: "Clubs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Standings_Tournaments_TournamentId",
+                table: "Standings",
+                column: "TournamentId",
+                principalTable: "Tournaments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ Repository/Migrations/20240425230513_Third.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Third : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Name", "Password", "UserName" },
+                values: new object[,]
+                {
+                    { 1, "pepe@mail.com", "Pepe", "123456", "pepe" },
+                    { 2, "pepito@mail.com", "Pepito", "456789", "pepito" },
+                    { 3, "pepon@mail.com", "Pepon", "111111", "pepon" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
