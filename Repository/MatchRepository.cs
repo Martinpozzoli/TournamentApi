3dc2f0445d3a0a7277e7f02f70cd3f718cdafbd1
leandrososa <52506861+leandrososa@users.noreply.github.com>
2024-04-29 13:24:59 -0300

    added match service

--- Repository/MatchRepository.cs
+++ Repository/MatchRepository.cs
@@ -7,9 +7,15 @@ namespace Repository
     public class MatchRepository : Repository<Match>, IMatchRepository
     {
         public MatchRepository(TournamentsDbContext context)
-            : base(context)
+            : base(context) { }
+
+        public async Task<Match> SetResult(int id, int score1, int score2)
         {
-            // TODO: Add tasks
+            var match = await _context.Matches.FindAsync(id);
+            match.ScoreTeamA = score1;
+            match.ScoreTeamB = score2;
+            await _context.SaveChangesAsync();
+            return match;
         }
     }
 }
